fb12aedf8f581ceb14eac220d6c40e764375e04d
kamran <kamranrasheed009@gmail.com>
2019-11-09 18:18:33 +0500

    assignment3

---
+++ resources/views/page.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <style>
+        
+            
+        </style>
+    </head>
+    <body>
+       @extends('welcome')
+       @section('mypage')
+       @parent
+       @endsection
+       @section('page2')
+       <h2>WELCOME TO PAGE 1</h2>
+
+    </body>
+</html>
---
+++ resources/views/page2.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <style>
+        
+            
+        </style>
+    </head>
+    <body>
+       @extends('page')
+       @section('page2')
+       @parent
+       @endsection
+       <h2>WELCOME TO PAGE 2</h2>
+
+    </body>
+</html>
